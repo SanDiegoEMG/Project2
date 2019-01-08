13cc10e0bea2210c1350dc4821d17a0383efda35
catmblake <catmblake@gmail.com>
2019-01-07 19:10:38 -0800

    added user model with first draft of user table structure

---
+++ models/user.js
@@ -0,0 +1,16 @@
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    user_name: DataTypes.STRING,
+    password: DataTypes.STRING,
+    app_type: DataTypes.STRING,
+    coding_time: DataTypes.INTEGER,
+    concept: DataTypes.STRING,
+    github: DataTypes.STRING,
+    linkedin: DataTypes.STRING,
+    profile: DataTypes.TEXT
+  });
+  return User;
+};

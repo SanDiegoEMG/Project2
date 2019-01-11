4e622c3dbfdbd1e888bc921a3dcca97e5ac4da9f
SD_emg <sandiegoemg@gmail.com>
2019-01-11 12:36:47 -0800

    style changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -37,16 +37,13 @@ module.exports = function (app) {
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
   app.post("/api/login", passport.authenticate("local"), function (req, res) {
-    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
-    // So we're sending the user back the route to the members page because the redirect will happen on the front end
-    // They won't get this or even be able to access this page if they aren't authed
+    // Since doing a POST with JS, can't redirect into a GET request - send user back to landing page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authorized
     res.json(req.user);
 
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
+  // Route for signing up a user. User's password is automatically hashed and stored securely bc of configuration of Sequelize User Model. If the user is created successfully, log them in, otherwise send back an error
   app.post("/api/signup", function (req, res) {
     // console.log(req.body);
     db.User.create(req.body)
